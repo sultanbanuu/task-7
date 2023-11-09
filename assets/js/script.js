88ef59bf674af9c8b2d14f59f801ce3cecd60c2e
sultanbanuu <sultanbanuahmett@gmail.com>
2023-11-09 14:16:29 +0400

    fixed problems v2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,3 @@
-// function myFunction() {
-//   var x = document.getElementById("myTopnav");
-//   if (x.className === "topnav") {
-//     x.className += " responsive";
-//   } else {
-//     x.className = "topnav";
-//   }
-// }
-
-
 const sign_up_btn = document.querySelector('#signUpBtn');
 const closeBtn = document.querySelector(".close");
 const modal = document.querySelector("#signUpModal");
